011873faad56c149e6962bcf27ccc097b13ae765
alan <im_wangshi@163.com>
2020-07-11 18:28:40 +0800

    practice with:https://www.chaindesk.cn/witbook/13/178

--- tmp.go
+++ tmp.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"reflect"
 	"strings"
+	"unicode"
 )
 
 // ppp 类型判断
@@ -20,9 +21,109 @@ func ppp(s string) {
 func ccc(s string) {
 	arr := strings.Split(s, ".")
 	fmt.Printf("slice len :%d \n", len(s))
-	fmt.Printf("The first unit8 of slice is: %s", arr[0])
+	fmt.Printf("The first unit8 of slice is: %s \n", arr[0])
+}
+
+// slice 拷贝
+func sliceCopy() {
+	a := []string{"a", "b", "c"}
+	// b := []string{"d", "e"}
+	c := []string{"f", "g", "h", "i", "j"}
+	copy(a[1:], c[:])
+	fmt.Println(a)
+	copy(c[1:], c[2:])
+	fmt.Println(c)
+}
+
+// 编码
+func sliceCode() {
+	// single := '\ufe35'
+	single := rune('1')
+	fmt.Printf("rune等价于%T ; single的码点为：%v \n", single, single)
+	fmt.Println(unicode.IsNumber(single))
+
+	s := []byte("abc     a aaa     ccc  ddd d")
+	fmt.Printf("[] byte's len: %d\n", len(s))
+	// fmt.Printf()
+	tORf := unicode.IsSpace(rune(s[1])) //空格判断
+	fmt.Printf("is space or not: %t\n", tORf)
+}
+
+type Point struct {
+	x, y int
+}
+
+// 结构体作为函数参数或返回值使用，如果考虑效率的话，结构体通常会用指针的方式传入和返回
+func Scale(p *Point, factor int) Point {
+	return Point{p.x * factor, p.y * factor}
+}
+
+// 返回url中最后一个"/"符号的索引
+func stringIndex() {
+	url := "http://www.omdbapi.com/?i=tt3896198"
+	pos := strings.LastIndex(url, "/")
+	fmt.Println(pos)
+}
+
+// const常量
+func const0711() {
+	x := 100
+	fmt.Println(&x)
+	x, y := 200, "abc"
+	fmt.Println(&x, x)
+	fmt.Print(y)
+	const (
+		a = iota
+		b
+		c = "haha"
+		d
+		e = iota
+	)
+	fmt.Println(a, b, c, d, e)
+	// 数字常量不会分配存储空间，无法寻址，故下一行的写法会报错
+	// fmt.Println(&e)
+}
+
+////////////////
+// switch条件语句 //
+////////////////
+func switch0711() {
+	// 局部变量
+	var grade string = "B"
+	var marks int = 90
+	switch marks {
+	case 90:
+		grade = "A"
+	case 80:
+		grade = "B"
+	case 50, 60, 70:
+		grade = "C"
+	default:
+		grade = "D"
+	}
+	switch {
+	case grade == "A":
+		fmt.Println("优秀！")
+	case grade == "B", grade == "C":
+		fmt.Println("良好！")
+	case grade == "D":
+		fmt.Printf("及格！")
+	case grade == "F":
+		fmt.Println("不及格！")
+	default:
+		fmt.Println("差！")
+	}
+	fmt.Printf("你的等级是：%s\n", grade)
 }
 func main() {
-	ppp("123abcdef")
-	ccc("tom.li")
+	// ppp("123abcdef")
+	// ccc("tom.li")
+	// sliceCopy()
+	// sliceCode()
+
+	// pp := &Point{1, 2}
+	// fmt.Println(Scale(pp, 5))
+	// stringIndex()
+	// const0711()
+	switch0711()
 }
