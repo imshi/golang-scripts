ddf347e2104f440cafe3c7cde96783f8f27ec9f8
shi.wang <im_alan@163.com>
2021-03-09 10:38:21 +0800

    add new

---
+++ go-by-example/45-regular-expression/regular-expression.go
@@ -0,0 +1,59 @@
+//  regexp包：内置正则表达式功能
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"regexp"
+)
+
+func main() {
+	// 测试一个字符串是否符合一个正则表达式，返回布尔值
+	match, _ := regexp.MatchString("p([a-z]+)ch", "peach")
+	fmt.Println(match)
+
+	// 若一个正则表达式要重复多次使用，可以预先编译正则结构体，使用的时候直接匹配
+	r, _ := regexp.Compile("p([a-z]+)ch")
+
+	// 多种预编译结构体方法
+	// 匹配测试，，返回布尔值
+	fmt.Println(r.MatchString("peach"))
+
+	// 查找字符串，匹配到返回匹配的内容，为匹配到返回空值
+	fmt.Println(r.FindString("peach punch"))
+
+	// 查找第一次匹配的字符串，返回匹配内容开始和结束位置索引
+	fmt.Println(r.FindStringIndex("peach punch"))
+
+	// Submatch 返回完全匹配和局部匹配的字符串，这里会返回 p([a-z]+)ch 和 ([a-z]+) 的 peach 和 ea
+	fmt.Println(r.FindStringSubmatch("peach punch"))
+
+	// 返回全匹配和局部匹配的索引位置
+	fmt.Println(r.FindStringSubmatchIndex("peach punch"))
+
+	// 带 All 的这个函数返回所有的匹配项，-1 表示不限制匹配次数
+	fmt.Println(r.FindAllString("peach punch pinch", -1))
+
+	// 带 All 的 Submatch 返回完全匹配和局部匹配的字符串的索引位置，-1 表示不限制匹配次数
+	fmt.Println(r.FindAllStringSubmatchIndex("peach punch pinch", -1))
+
+	// 提供一个正整数来限制匹配次数
+	fmt.Println(r.FindAllString("peach punch pinch", 2))
+
+	// MatchString 的等价用法： []byte参数 + 不带 String 的函数
+	fmt.Println(r.Match([]byte("peach")))
+
+	// MustCompile： Compile 的变体，Compile 返回两个值，不能用于常量，
+	r = regexp.MustCompile("p([a-z]+)ch")
+	fmt.Println("MustCompile:", r)
+
+	// 替换部分字符串为其他值
+	fmt.Println(r.ReplaceAllString("a peach", "<fruit>"))
+
+	// Func 变量允许传递匹配内容到一个给定的函数中
+	in := []byte("a peach")
+	out := r.ReplaceAllFunc(in, bytes.ToUpper)
+	fmt.Println(string(out))
+
+}
---
+++ go-by-example/46-json/json.go
@@ -0,0 +1,90 @@
+// encoding/json包：内置的 JSON 编解码支持，提供内置或者自定义类型与 JSON 数据之间的转化的功能
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+// 自定义类型的编码和解码
+type Response1 struct {
+	Page   int
+	Fruits []string
+}
+
+type Response2 struct {
+	Page   int      `json:"page"`
+	Fruits []string `json:"fruits"`
+}
+
+func main() {
+
+	// 基本数据类型到JSON字符串的编码
+	bolB, _ := json.Marshal(true)
+	fmt.Println("内置布尔类型编码：", string(bolB))
+
+	intB, _ := json.Marshal(1)
+	fmt.Println("内置整型编码：", string(intB))
+
+	fltB, _ := json.Marshal(2.34)
+	fmt.Println("内置浮点型编码：", string(fltB))
+
+	strB, _ := json.Marshal("gopher")
+	fmt.Println("内置字符串类型编码：", string(strB))
+
+	slcD := []string{"apple", "peach", "pear"}
+	slcB, _ := json.Marshal(slcD)
+	fmt.Println("内置切片类型编码：", string(slcB))
+
+	mapD := map[string]int{"apple": 5, "lettuce": 7}
+	mapB, _ := json.Marshal(mapD)
+	fmt.Println("内置map类型编码：", string(mapB))
+
+	// JSON 包可以自动编码你的自定义类型，仅输出可导出的字段，并且默认使用他们的名字作为 JSON 数据的键
+	res1D := &Response1{
+		Page:   1,
+		Fruits: []string{"apple", "peach", "pear"}}
+	res1B, _ := json.Marshal(res1D)
+	fmt.Println("自定义struct类型编码：", string(res1B))
+
+	// 通过在结构字段声明标签来自定义编码的 JSON 数据键名称
+	res2D := Response2{
+		Page:   2,
+		Fruits: []string{"apple", "peach", "pear"}}
+	res2B, _ := json.Marshal(res2D)
+	fmt.Println(string(res2B))
+
+	// 以下为解码：将 JSON 数据转化为 Go 值（变量）
+	byt := []byte(`{"num":6.13, "strs":["a","b"]}`)
+
+	// 提供一个存储解码数据的变量
+	var dat map[string]interface{}
+
+	// 实际的解码和相关的错误检查
+	if err := json.Unmarshal(byt, &dat); err != nil {
+		panic(err)
+	}
+	fmt.Println(dat)
+
+	// 为了使用解码 map 中的值（声明时 value为空接口），我们需要将他们进行适当的类型转换。例如这里我们将 num 的值转换成 float64类型
+	num := dat["num"].(float64)
+	fmt.Println(num)
+
+	// 访问嵌套的值需要一系列的转化
+	strs := dat["strs"].([]interface{})
+	str1 := strs[0].(string)
+	fmt.Println(str1)
+
+	// 解码 JSON 值到自定义类型：好处就是可以为我们的程序带来额外的类型安全加强，并且消除在访问数据时的类型断言
+	str := `{"page":1, "fruits": ["apple", "peach"]}`
+	res := &Response2{}
+	json.Unmarshal([]byte(str), &res)
+	fmt.Println(res)
+	fmt.Println(res.Fruits[0])
+
+	// 上面的例子中，我们经常使用 byte 和 string 作为使用标准输出时数据和 JSON 表示之间的中间值。我们也可以和os.Stdout 一样，直接将 JSON 编码直接输出至 os.Writer流中，或者作为 HTTP 响应体
+	enc := json.NewEncoder(os.Stdout)
+	d := map[string]int{"apple": 5, "lettuce": 7}
+	enc.Encode(d)
+}
