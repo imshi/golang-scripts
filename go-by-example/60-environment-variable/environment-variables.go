6449023bb1019f95c2808d1ad28742edf3221e24
shi.wang <im_alan@163.com>
2021-03-16 11:40:19 +0800

    update comm

--- go-by-example/60-environment-variable/environment-variables.go
+++ go-by-example/60-environment-variable/environment-variables.go
@@ -11,14 +11,14 @@ import (
 func main() {
 
 	// 使用 os.Sentenv设置一个键值对；
-	// 使用 os.Getenv获取一个对应的键值对（不存在的话返回一个空字符串）；
-	// 使用 os.Environ列出所有环境变量的键值对
 	os.Setenv("FOO", "1")
+	// 使用 os.Getenv获取一个对应的键值对（不存在的话返回一个空字符串）；
 	fmt.Println("FOO:", os.Getenv("FOO"))
 	fmt.Println("BAR:", os.Getenv("BAR"))
 
 	fmt.Println()
 	// 遍历系统环境变量
+	// 使用 os.Environ列出所有环境变量的键值对
 	for _, e := range os.Environ() {
 		// 提取并打印键名
 		pair := strings.Split(e, "=")
