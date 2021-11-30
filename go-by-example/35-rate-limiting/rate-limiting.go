1c20dae2aed2d0dc5655f08d7f169e8e78562ed7
shi.wang <im_alan@163.com>
2021-11-30 11:28:37 +0800

    update comment

--- go-by-example/35-rate-limiting/rate-limiting.go
+++ go-by-example/35-rate-limiting/rate-limiting.go
@@ -9,8 +9,8 @@ import (
 )
 
 // 该程序运行效果为：
-// 第一批请求意料之中的大约每 200ms 处理一次
-// 第二批请求，直接连续处理了 3 次，这是由于这个“脉冲”速率控制，然后大约每 200ms 处理其余的 2 个
+// 第一批请求：大约每 1000ms 处理一次
+// 第二批请求：直接连续处理了 3 次，然后大约每 1000ms 一次处理其余的 2 个请求
 func main() {
 	requests := make(chan int, 5)
 	for i := 0; i < 5; i++ {
@@ -19,32 +19,33 @@ func main() {
 
 	close(requests)
 
-	// 这个 limiter 通道将每 200ms 接收一个值
-	limiter := time.Tick(time.Millisecond * 200)
+	// 这个 limiter 通道将每 1000ms 接收一个值
+	limiter := time.Tick(time.Millisecond * 1000)
 
 	for req := range requests {
-		// 通过在每次请求前阻塞 limiter 通道的一个接收限制自己每 200ms 执行一次请求
+		// 通过在每次请求前阻塞 limiter 通道的一个接收， 可以将频率限制为：每 1000ms 执行一次请求
 		<-limiter
 		fmt.Println("requests: ", req, time.Now())
 	}
 
-	// 如果需要临时进行速率限制，并且不影响整体的速率控制我们可以通过通道缓冲来实现
-	// 此处 burstyLimiter 通道用来进行 3 次临时的脉冲型速率限制
+	fmt.Println("-------------------------------------")
+	// 如果需要临时调整速率限制，并且不影响整体的速率控制，可以通过通道缓冲来实现
+	// 此处 burstyLimiter 通道允许最多 3 个爆发（bursts）事件
 	burstyLimiter := make(chan time.Time, 3)
 
-	// 想将通道填充需要临时改变3次的值
+	// 填充通道，表示允许的爆发（bursts）
 	for i := 0; i < 3; i++ {
 		burstyLimiter <- time.Now()
 	}
 
-	// 每 200 ms 添加一个新的值到 burstyLimiter中，直到达到 3 个的限制
+	// 每 1000 ms 添加一个新的值到 burstyLimiter中，直到达到 3 个的限制
 	go func() {
-		for t := range time.Tick(time.Millisecond * 200) {
+		for t := range time.Tick(time.Millisecond * 1000) {
 			burstyLimiter <- t
 		}
 	}()
 
-	// 模拟超过 5 个的接入请求。它们中前 3 个将由于受 burstyLimiter 的“脉冲”影响
+	// 模拟 5 个传入请求；受益于 burstyLimiter 的爆发（bursts）能力，前 3 个请求可以快速完成
 	burstyRequests := make(chan int, 5)
 	for i := 0; i < 5; i++ {
 		burstyRequests <- i
